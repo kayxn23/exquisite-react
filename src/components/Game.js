d40acbf46033aba9032316b9b8f80eb70a5cb6fb
Kay <kayxn23@gmail.com>
2018-12-11 14:55:12 -0800

    set up state in Game and submitLine function created

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,6 +8,20 @@ class Game extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      index: 1,
+      submissions: [],
+      isSubmitted: false,
+    }
+  }
+
+  submitLine = (newLine) => {
+    this.setState({index: this.state.index + 1 })
+
+    const submissions = this.state.submissions;
+    submissions.push(newLine)
+    this.setState({submissions: submissions})
   }
 
   render() {
@@ -34,7 +48,7 @@ class Game extends Component {
 
         <RecentSubmission />
 
-        <PlayerSubmissionForm />
+        <PlayerSubmissionForm fields={FIELDS} index={this.state.index} sendSubmissionCallback={this.submitLine}/>
 
         <FinalPoem />
 
